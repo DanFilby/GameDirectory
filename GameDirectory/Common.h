f999f2480920bc0b4d4e9b450c0970857fe5278c
Daniel Filby <df245193@falmouth.ac.uk>
2023-06-09 00:19:47 +0100

    added common.h
    contains smart pointer header setup

---
+++ GameDirectory/Common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#pragma region SmartPointers
+
+#include <memory>
+
+using std::unique_ptr;
+using std::shared_ptr;
+
+using std::make_shared;
+using std::make_unique;
+
+using std::dynamic_pointer_cast;
+using std::static_pointer_cast;
+
+#pragma endregion
+
+
--- GameDirectory/Source.cpp
+++ GameDirectory/Source.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+#include "Common.h"
+#include "EntryDatabase.h"
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    auto ge = make_shared<GameEntry>(); 
+    auto entry = dynamic_pointer_cast<Entry>(ge);
+
+    std::cout << "game entry id: " << entry->ID() << "\n";
+    std::cout << "game entry rating: " << ge->Rating();
+
 }
 
